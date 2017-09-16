12db2e033fc587bf883c9cb19f03c267d36c8b1e
Sebastian <sebastian.schuepbach@unibas.ch>
2017-09-16 16:50:43 +0200

    Edoc documents which contain DOI should be filtered out

---
+++ src/main/scala/io/github/dataramblers/EdocStreaming.scala
@@ -0,0 +1,23 @@
+package io.github.dataramblers
+
+import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.api.scala._
+
+
+object EdocStreaming {
+
+  def main(args: Array[String]): Unit = {
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    val edocRecords = env.readTextFile("/home/schuepbs/temp/hackathon17/all.small.json.gz")
+
+    val test = edocRecords
+      .map(x => JsonParser.toEdoc(x))
+      .filter(x => x.doi.isEmpty)
+      .map(x => ESLookup.lookup(x, "crossref", "crossref"))
+    //
+
+    test.writeAsText(".")
+  }
+
+}
